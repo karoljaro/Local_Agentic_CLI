eb4b1bd2dc079d169b33a9c62e4d213b85dd6599
karoljaro <111643413+karoljaro@users.noreply.github.com>
2026-06-09 19:47:03 +0200

    feat: Add temporal-polyfill dependency and update TemporalClock to use it

--- src/infrastructure/runtime/TemporalClock.ts
+++ src/infrastructure/runtime/TemporalClock.ts
@@ -1,10 +1,15 @@
 import type { ClockPort } from "@/application/ports/ClockPort";
 import { asISODateTime, type ISODateTime } from "@/domain/Ids";
+import { Temporal } from "temporal-polyfill";
 
 export class TemporalClock implements ClockPort {
 	now(): ISODateTime {
-		if (typeof Temporal === "undefined") {
-			return asISODateTime(new Date().toISOString());
+		// TODO: If temporal will work on the bun.js runtime, replace temporal-polyfill with the official temporal provided by the bun.js runtime
+		if (typeof Temporal === 'undefined') {
+			// return asISODateTime(new Date().toISOString());
+			throw new Error(
+				'Temporal API is not available in this environment.'
+			);
 		}
 
 		return asISODateTime(Temporal.Now.instant().toString());
